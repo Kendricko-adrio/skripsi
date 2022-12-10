69275a2cc4cd749d307da66ab656134ffe1c41e2
Kendricko-adrio <kendrickoadrio134@gmail.com>
2022-12-10 17:40:42 +0700

    course service done

---
+++ service/course-skripsi/src/main/java/com/example/courseskripsi/model/Course.java
@@ -0,0 +1,35 @@
+package com.example.courseskripsi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.math.BigInteger;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "courses")
+public class Course extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private BigInteger id;
+
+    @Column(name = "course_name")
+    private String courseName;
+
+    @Column(name = "course_description")
+    private String courseDescription;
+
+    @Column(name = "course_price")
+    private BigInteger coursePrice;
+
+    @Column(name = "created_by")
+    private BigInteger createdBy;
+}
---
+++ service/course-skripsi/src/test/java/com/example/courseskripsi/CourseSkripsiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.courseskripsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSkripsiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
