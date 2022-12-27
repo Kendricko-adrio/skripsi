232e746e7952935a060ed240c8e931904d19aea0
michaelianto <mmichaelchristian2231@gmail.com>
2022-12-27 15:31:04 +0700

    add readNotifications method

--- monolith/src/main/java/com/skripsi/monolith/controller/notification/NotificationController.java
+++ monolith/src/main/java/com/skripsi/monolith/controller/notification/NotificationController.java
@@ -5,6 +5,7 @@ import com.skripsi.monolith.model.user.User;
 import com.skripsi.monolith.service.notification.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
@@ -29,5 +30,10 @@ public class NotificationController {
 
   @SchemaMapping List<Notification> notifications(User user) {
     return notificationService.getNotifications(user.getId());
+  }
+
+  @MutationMapping
+  public Boolean readNotifications(@Argument List<BigInteger> ids){
+    return notificationService.readNotifications(ids);
   }
 }
